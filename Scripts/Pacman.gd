8d6fea1630fec9ece34dc8739fa7e5d1a72c3850
Buitre-Slayer <saacmanjarrez@gmail.com>
2020-05-21 07:24:55 -0500

    Movimiento de jugador correcto

--- Scripts/Pacman.gd
+++ Scripts/Pacman.gd
@@ -1,36 +1,62 @@
 extends KinematicBody2D
 onready var pacman=$"."
 onready var animationPlayer=$AnimationPlayer
-export var velocidad=1
-enum DIRECCION { arriba=0, derecha=1, abajo=2, izquierda=3}
-var direccion
-func _ready():
-	animationPlayer.play("Posicion inicial")
+onready var rayCast=$RayCast2D
+export var velocidad=64
+enum DIRECCION {sinDireccion=0 ,arriba=1, derecha=2, abajo=3, izquierda=4}
+var mapDir=[Vector2.ZERO,Vector2.UP,Vector2.RIGHT,Vector2.DOWN,Vector2.LEFT]
+var mapAnim=["Posicion inicial","Caminar arriba","Caminar derecha","Caminar abajo","Caminar izquierda"]
+var direccion=DIRECCION.sinDireccion
+var tamTile=64
+var posActual:=Vector2(position.x,position.y)
+var posSig:=Vector2(position.x,position.y)
 
-func LeerEntradas():
+func _ready():
+	animationPlayer.play(mapAnim[direccion])
+
+func ObtenerDireccion():
 	if Input.is_action_pressed("ui_up"):
-		animationPlayer.play("Caminar arriba")
 		direccion=DIRECCION.arriba
 	if Input.is_action_pressed("ui_right"):
-		animationPlayer.play("Caminar derecha")
 		direccion=DIRECCION.derecha
 	if Input.is_action_pressed("ui_down"):
-		animationPlayer.play("Caminar abajo")
 		direccion=DIRECCION.abajo
 	if Input.is_action_pressed("ui_left"):
-		animationPlayer.play("Caminar izquierda")
 		direccion=DIRECCION.izquierda
 
+func PosicionLibre(pos)->bool:
+	rayCast.set_cast_to(pos)
+	rayCast.force_raycast_update()
+	if rayCast.is_colliding(): return false
+	return true
+
+
 func Mover():
-	if direccion==DIRECCION.arriba:
-		move_and_collide(Vector2(0,-velocidad))
-	elif direccion==DIRECCION.derecha:
-		move_and_collide(Vector2(velocidad,0))
-	elif direccion==DIRECCION.abajo:
-		move_and_collide(Vector2(0,velocidad))
-	elif direccion==DIRECCION.izquierda:
-		move_and_collide(Vector2(-velocidad,0))
+	if posActual!=posSig:
+		move_and_collide(mapDir[direccion]*get_physics_process_delta_time()*velocidad)
+		if pacman.position.distance_to(posActual)>=tamTile: pacman.position=posSig
+
+	if pacman.position==posSig:
+		var dirAnterior=direccion
+		ObtenerDireccion()
+		posActual=posSig
+		posSig=posActual+mapDir[direccion]*tamTile
+		if !PosicionLibre(mapDir[direccion]*tamTile): 
+			direccion=dirAnterior
+			posSig=posActual+mapDir[direccion]*tamTile
+			if !PosicionLibre(mapDir[direccion]*tamTile):
+				posSig=posActual
+		else: animationPlayer.play(mapAnim[direccion])
+	else:
+		var dirAnterior=direccion
+		ObtenerDireccion()
+		if posSig+mapDir[direccion]*tamTile==posActual:
+			var aux
+			aux=posActual
+			posActual=posSig
+			posSig=aux
+			animationPlayer.play(mapAnim[direccion])
+		else: direccion=dirAnterior
 
 func _physics_process(delta):
-	LeerEntradas()
 	Mover()
