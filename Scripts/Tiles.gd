d4cff9f3c208292260c803e18810f3fa336bc8f5
Buitre-Slayer <saacmanjarrez@gmail.com>
2020-05-23 06:20:45 -0500

    Comida y tiles combinados, funcionamiento correcto de la comida

--- Scripts/Tiles.gd
+++ Scripts/Tiles.gd
@@ -6,15 +6,16 @@ func _ready():
 func PonerComida():
 	var aumento=64
 	var offset=32
-	var escenaComida=5 #preload()
+	var escenaComida=preload("res://Escenas/Comida.tscn")
 	var i=0
 	var j=0
 	while get_cellv(world_to_map(Vector2(i,j)))!=-1:
-		j=0
 		while get_cellv(world_to_map(Vector2(i,j)))!=-1:
+			print(get_cellv(world_to_map(Vector2(i,j))))
 			if get_cellv(world_to_map(Vector2(i,j)))==1:
 				var comida=escenaComida.instance()
-				comida.set_pos(Vector2(i+offset,j+offset))
+				comida.position=Vector2(i+offset,j+offset)
 				add_child(comida)
 			j+=aumento
 		i+=aumento
+		j=0
