ed42539dbf0aa67f8e9ca49f3aac310b51586b1d
Buitre-Slayer <saacmanjarrez@gmail.com>
2020-06-09 01:10:46 -0500

    Actualizaciones de mapa de forma global y no por fantasma, Fantasmas: funciones de persecucion y huida de pacman

--- Scripts/Fantasmas/BaseFantasmas.gd
+++ Scripts/Fantasmas/BaseFantasmas.gd
@@ -20,11 +20,51 @@ func Mover(delta: float)-> void:
 		position= posSig
 		posActual= posSig
 
-func ObtenerDireccion()-> Vector2:
-	return Vector2.ZERO
+func ObtenerDireccionPacman()-> Vector2:
+	var posCuadricula: Vector2= gMapa.tile.world_to_map(position)
+	var auxPosCuadricula: Vector2
+	var disMenor: int= 1000000
+	var dirMenor: Vector2= Vector2.ZERO
+	var i=0
+	while i<4:
+		auxPosCuadricula= position
+		auxPosCuadricula.x+= gMapa.movs[i][0]
+		auxPosCuadricula.y+= gMapa.movs[i][1]
+		auxPosCuadricula=gMapa.tile.world_to_map(auxPosCuadricula)
+		if gMapa.distancia[auxPosCuadricula.y][auxPosCuadricula.x]< disMenor:
+			disMenor= gMapa.distancia[auxPosCuadricula.y][auxPosCuadricula.x]
+			dirMenor=posCuadricula.direction_to(auxPosCuadricula)
+		i+=1
+	return dirMenor
 
-func ObtenerSiguientePosicion()-> void:
-	pass
+func ObtenerSiguientePosicionPacman()-> void:
+	direccion= ObtenerDireccionPacman()
+	if direccion!= Vector2.ZERO:
+		posSig= posActual+ direccion* 64
+		animacion.play(mapAnim[direccion])
+
+func ObtenerDireccionHuida():
+	var posCuadricula: Vector2= gMapa.tile.world_to_map(position)
+	var auxPosCuadricula: Vector2
+	var disMenor: int= 0
+	var dirMenor: Vector2= Vector2.ZERO
+	var i=0
+	while i<4:
+		auxPosCuadricula= position
+		auxPosCuadricula.x+= gMapa.movs[i][0]
+		auxPosCuadricula.y+= gMapa.movs[i][1]
+		auxPosCuadricula=gMapa.tile.world_to_map(auxPosCuadricula)
+		if gMapa.distancia[auxPosCuadricula.y][auxPosCuadricula.x]!=1000000 and  gMapa.distancia[auxPosCuadricula.y][auxPosCuadricula.x]> disMenor:
+			disMenor= gMapa.distancia[auxPosCuadricula.y][auxPosCuadricula.x]
+			dirMenor=posCuadricula.direction_to(auxPosCuadricula)
+		i+=1
+	return dirMenor
+
+func ObtenerSiguientePosicionHuida():
+	direccion= ObtenerDireccionHuida()
+	if direccion!= Vector2.ZERO:
+		posSig= posActual+ direccion* 64
+		animacion.play(mapAnim[direccion])
 
 func _physics_process(delta: float) -> void:
 	if gPacman.powerUp:
--- Scripts/Fantasmas/CAZADOR.gd
+++ Scripts/Fantasmas/CAZADOR.gd
@@ -1,29 +1,7 @@
 extends "res://Scripts/Fantasmas/BaseFantasmas.gd"
 
-func ObtenerDireccion()-> Vector2:
-	var posCuadricula: Vector2= gMapa.tile.world_to_map(position)
-	var auxPosCuadricula: Vector2
-	var disMenor: int= 1000000
-	var dirMenor: Vector2= Vector2.ZERO
-	var i=0
-	while i<4:
-		auxPosCuadricula= position
-		auxPosCuadricula.x+= gMapa.movs[i][0]
-		auxPosCuadricula.y+= gMapa.movs[i][1]
-		auxPosCuadricula=gMapa.tile.world_to_map(auxPosCuadricula)
-		if gMapa.distancia[auxPosCuadricula.y][auxPosCuadricula.x]< disMenor:
-			disMenor= gMapa.distancia[auxPosCuadricula.y][auxPosCuadricula.x]
-			dirMenor=posCuadricula.direction_to(auxPosCuadricula)
-		i+=1
-	return dirMenor
-
-func ObtenerSiguientePosicion()-> void:
-	gMapa.Mapear(gPacman.posicion, position)
-	direccion= ObtenerDireccion()
-	if direccion!= Vector2.ZERO:
-		posSig= posActual+ direccion* 64
-		animacion.play(mapAnim[direccion])
-
 func _physics_process(delta: float) -> void:
 	if posActual!= posSig: Mover(delta)
-	if posActual== posSig: ObtenerSiguientePosicion()
+	if posActual== posSig: 
+		if not gPacman.powerUp: ObtenerSiguientePosicionPacman()
+		else: ObtenerSiguientePosicionHuida()
