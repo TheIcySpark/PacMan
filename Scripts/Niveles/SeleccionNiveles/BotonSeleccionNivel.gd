be8e1aec3b6d463c5b6494373c4865890d42b308
Buitre-Slayer <saacmanjarrez@gmail.com>
2020-06-10 22:26:21 -0500

    diseño escena niveles: Mejor diseño

--- Scripts/Niveles/SeleccionNiveles/BotonSeleccionNivel.gd
+++ Scripts/Niveles/SeleccionNiveles/BotonSeleccionNivel.gd
@@ -1,6 +1,8 @@
 extends TextureButton
 export var escena: String
+export var nivel: int
 onready var estrella: TextureRect= $Completado
+onready var etiquetaNivel: Label= $Nivel
 var disponible: bool= false
 var completado: bool= false
 
@@ -8,6 +10,7 @@ func _ready() -> void:
 	Inicializar()
 
 func Inicializar()-> void:
+	etiquetaNivel.set_text(str(nivel))
 	if completado: estrella.set_modulate(Color( 255, 255, 255))
 	if disponible: set_disabled(false)
 
