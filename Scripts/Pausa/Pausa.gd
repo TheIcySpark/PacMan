8e8c7b291fea8ff88cc55da20275c773f0cfc846
Buitre-Slayer <saacmanjarrez@gmail.com>
2020-06-12 20:16:23 -0500

    Pausa: mejor diseño en pausa

--- Scripts/Pausa/Pausa.gd
+++ Scripts/Pausa/Pausa.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 onready var control: Control= $Control
 
-func _on_QuitarPausa_button_up() -> void:
+func _on_Continuar_button_up() -> void:
 	get_tree().set_pause(false)
 	control.visible= false
+
+
+func _on_Salir_button_up() -> void:
+	gMapa.tile= null
+	gCambiadorEscena.CambiarEscena("res://Escenas/Niveles/Seleccion nivel/SeleccionNivel.tscn")
